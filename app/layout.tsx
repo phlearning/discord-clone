13d1ea2c127844a033d98cf77d3dc81ceb9a7218
Pierre-Henri Vong <pierrehenrivong@gmail.com>
2024-11-25 14:14:02 +0100

    Ep 05: Dark & Light Mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { ThemeProvider } from '@/components/providers/theme-providers'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -16,8 +18,19 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider afterSignOutUrl="/">
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(
+          font.className,
+          "bg-white dark:bg-discord-dark"
+          )}>
+          <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          storageKey="discord-theme">
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
 
     </ClerkProvider>
