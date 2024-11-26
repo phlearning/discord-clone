3746ec439f66fbd205a95842efeb4b4ebb80365a
Pierre-Henri Vong <pierrehenrivong@gmail.com>
2024-11-26 14:19:36 +0100

    Ep 08: UploadThing Setup

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 ## env example
 
-Get your api key from Clerk for the authentication then paste in the `.env`
-```
-NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_...
-CLERK_SECRET_KEY=sk_test_...
-```
+- Get your api key from Clerk for the authentication then paste in the `.env`
+    ```
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_...
+    CLERK_SECRET_KEY=sk_test_...
+    ```
+- Add the redirect and clerk sign in url folders in the `.env`:
+    ```
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL=/sign-in
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL=/sign-up
+    NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL=/
+    NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL=/
+    ```
+- Get your api key from Neondb for the Database:
+    ```
+    DATABASE_URL="postgresql://neondb_owner:..."
+    ```
+- Get the api key for the upload from Uploadthing:
+    ```
+    UPLOADTHING_TOKEN=...
+    ```
+
 
 ## Getting Started
 
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -3,6 +3,8 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
+
 import {
     Dialog,
     DialogContent,
@@ -23,7 +25,7 @@ import {
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -77,7 +79,21 @@ export const InitialModal = () => {
                         className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image upload
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload 
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
 
                             <FormField
