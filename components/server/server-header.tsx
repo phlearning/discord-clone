1e42e228f0924ef900ebe3d0b02dc7c59d7f602f
Pierre-Henri Vong <pierrehenrivong@gmail.com>
2024-11-27 17:05:45 +0100

    Ep 17: Delete & Leave server modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -88,6 +88,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("deleteServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Delete Server
@@ -96,6 +97,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("leaveServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
                     >
                         Leave Server
