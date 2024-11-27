cf4943d7b8516fc4c580d5bc87a521d69a75185f
Pierre-Henri Vong <pierrehenrivong@gmail.com>
2024-11-27 15:34:48 +0100

    Ep 15: Manage Members

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -11,7 +11,6 @@ import {
     DialogTitle,
 } from "@/components/ui/dialog";
 
-import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
