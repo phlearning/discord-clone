5effbadb22091007d803d6220d6f85dec1df34ac
Pierre-Henri Vong <pierrehenrivong@gmail.com>
2024-12-03 15:08:43 +0100

    Ep 33: Chat Socket Hook

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -7,6 +7,7 @@ import qs from "query-string";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useState } from "react";
+import { useRouter, useParams } from "next/navigation";
 import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import { Member, MemberRole, Profile } from "@prisma/client";
 
@@ -62,6 +63,16 @@ export const ChatItem = ({
 }: ChatItemProps) => {
     const [isEditing, setIsEditing] = useState(false);
     const { onOpen } = useModal();
+    const params = useParams();
+    const router = useRouter();
+
+    const onMemberClick = () => {
+        if (member.id === currentMember.id) {
+            return;
+        }
+
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`);
+    }
 
     useEffect(() => {
         const handleKeyDown = (event: any) => {
@@ -70,9 +81,9 @@ export const ChatItem = ({
             }
         };
 
-        window.addEventListener("keyDown", handleKeyDown);
+        window.addEventListener("keydown", handleKeyDown);
 
-        return () => window.removeEventListener("keyDown", handleKeyDown);
+        return () => window.removeEventListener("keydown", handleKeyDown);
     }, [])
 
     const form = useForm<z.infer<typeof formSchema>>({
@@ -143,13 +154,17 @@ export const ChatItem = ({
         <div className="relative group flex items-center hover:bg-black/5
          p-4 transition w-full">
             <div className="group flex gap-x-2 items-start w-full">
-                <div className="cursor-pointer hover:drop-shadow-md transition">
+                <div
+                    onClick={onMemberClick} 
+                    className="cursor-pointer hover:drop-shadow-md transition">
                     <UserAvatar src={member.profile.imageUrl} />
                 </div>
                 <div className="flex flex-col w-full">
                     <div className="flex items-center gap-x-2">
                         <div className="flex items-center">
-                            <p className="font-semibold text-sm hover:underline
+                            <p 
+                                onClick={onMemberClick}
+                                className="font-semibold text-sm hover:underline
                              cursor-pointer">
                                 {member.profile.name}
                             </p>
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -7,6 +7,7 @@ import { ChatWelcome } from "./chat-welcome";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { Loader2, ServerCrash } from "lucide-react";
 import { ChatItem } from "./chat-item";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm";
 
@@ -40,6 +41,8 @@ export const ChatMessages = ({
     type
 }: ChatMessagesProps) => {
     const queryKey = `chat:${chatId}`;
+    const addKey = `chat:${chatId}:messages`;
+    const updateKey = `chat:${chatId}:messages:update`
 
     const {
         data,
@@ -53,8 +56,9 @@ export const ChatMessages = ({
         paramKey,
         paramValue
     });
+    useChatSocket({ queryKey, addKey, updateKey });
 
-    if (status === "loading") {
+    if (status === "pending") {
         return (
             <div className="flex flex-col flex-1 justify-center items-center">
                 <Loader2 className="h-7 w-7 text-zinc-500 animate-spin my-4" />
